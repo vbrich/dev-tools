38fac8cbc66aa27206b41e118a408bd9e927ef78
vbrich <vb_rich@yahoo.com>
2022-12-12 12:10:50 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {CssBaseline} from '@mui/material';
+import CustomThemeProvider from './themes/CustomThemeProvider'
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <CustomThemeProvider>
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </CustomThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/themes/dark.js
@@ -0,0 +1,40 @@
+import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+// A custom theme for this app
+const theme_dark = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#61dafb',
+      light: '#61dafb',
+      dark: '#21a1c4',
+    },
+    secondary: {
+      main: '#b5ecfb',
+      light: '#61dafb',
+      dark: '#21a1c4',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#000',
+    },
+  },
+  overrides: {
+    MuiPaper: {
+      root: {
+        padding: '20px 10px',
+        margin: '10px',
+        backgroundColor: '#000', // 5d737e
+      },
+    },
+    MuiButton: {
+      root: {
+        margin: '5px',
+      },
+    },
+  },
+});
+export default theme_dark;
